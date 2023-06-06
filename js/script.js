c5f8444be140d37d749e61dee95c09329911fd31
device-vlados <102617814+device-vlados@users.noreply.github.com>
2023-06-06 22:19:27 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -30,26 +30,28 @@ if (animItems.length > 0) {
 }
 
 $(window).scroll(function() {
+    setTimeout(function() {
     var $window = $(window),
         $body = $('body'),
         $panel = $('.panel');
-    
+
     var scroll = $window.scrollTop() + ($window.height() / 3);
 
     $panel.each(function () {
         var $this = $(this);
 
         if ($this.position().top <= scroll && $this.position().top + $this.height() > scroll) {
-            
+
         $body.removeClass(function (index, css) {
             return (css.match (/(^|\s)color-\S+/g) || []).join(' ');
         });
 
         $body.addClass('color-' + $(this).data('color'));
         }
-    });    
-    
+    });
+    }, 500);
 }).scroll();
+
 
 
 
